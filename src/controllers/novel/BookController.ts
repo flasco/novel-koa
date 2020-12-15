220b89e51e85aabf5707361cbaad42f39e6715d7
czq <chenzhengqiang@youzan.com>
2020-12-15 21:01:49 +0800

    fix: update

---
+++ src/controllers/novel/BookController.ts
@@ -0,0 +1,26 @@
+import { Controller, Description, Get, Post } from 'pricking-koa/dist/utils/decorator';
+
+import BaseController from '@app/controllers/common/IndexController';
+
+@Controller('/v3/books')
+class AnalyseController extends BaseController {
+  @Get('/info')
+  @Description('查询书籍详情')
+  async getBookInfo() {
+    this.ctx.success({});
+  }
+
+  @Get('/search')
+  @Description('搜索')
+  async searchBook() {
+    this.ctx.success({});
+  }
+
+  @Post('/origin')
+  @Description('查询书籍书源详情')
+  async getBookOriginDetail() {
+    this.ctx.success({});
+  }
+}
+
+export = AnalyseController;
