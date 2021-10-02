79905ab7497ac7f668c17294b3d2c4a242c951e8
flasco <flasco@163.com>
2021-10-02 22:36:10 +0800

    fix: 修复build-scirpt

--- scripts/build.ts
+++ scripts/build.ts
@@ -3,6 +3,6 @@ import { $, fs } from 'zx';
 void (async function () {
   await fs.remove('dist');
   await $`yarn tsc -p tsconfig.build.json`;
-  await $`yarn tscpaths -p tsconfig.build.json -s ./src -o ./dist/src`;
+  await $`yarn tscpaths -p tsconfig.build.json -s ./src -o ./dist`;
   await fs.copySync('src/views', 'dist/views');
 })();
