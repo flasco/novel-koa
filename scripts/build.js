3a4ace2575c4c4ee41e9fb9db3138dccdbb7b905
flasco <flasco@163.com>
2021-10-29 00:47:20 +0800

    fix: use swc

--- scripts/build.js
+++ scripts/build.js
@@ -2,7 +2,8 @@ const { $, fs } = require('zx');
 
 (async function () {
   await fs.remove('dist');
-  await $`yarn tsc -p tsconfig.build.json`;
+  // await $`yarn tsc -p tsconfig.build.json`;
+  await $`yarn swc ./src -d ./dist`;
   await $`yarn tscpaths -p tsconfig.build.json -s ./src -o ./dist`;
   fs.copySync('src/views', 'dist/views');
   const pkg = fs.readJSONSync('./package.json');
