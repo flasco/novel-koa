63ee0ef8bdf484e4ab196848a1ebda20d7c2ccc0
flasco <flasco@163.com>
2022-03-31 22:18:46 +0800

    fix: change

--- src/middlewares/cross-origin.ts
+++ src/middlewares/cross-origin.ts
@@ -1,12 +1,12 @@
-const allowedSite = process.env.APP_ENV === 'development' ? '*' : 'https://*.avosapps.us';
-
 export = () => async (ctx, next) => {
-  ctx.set('Access-Control-Allow-Credentials', true);
-  ctx.set('Access-Control-Allow-Origin', allowedSite);
-  // 允许的header类型
-  ctx.set('Access-Control-Allow-Headers', 'x-requested-with,content-type');
-  // 跨域允许的请求方式
-  ctx.set('Access-Control-Allow-Methods', 'DELETE,PUT,POST,GET,OPTIONS');
+  if (process.env.APP_ENV === 'development' || /tassel-\d.avosapps.us/.test(ctx.host)) {
+    ctx.set('Access-Control-Allow-Credentials', true);
+    ctx.set('Access-Control-Allow-Origin', '*');
+    // 允许的header类型
+    ctx.set('Access-Control-Allow-Headers', 'x-requested-with,content-type');
+    // 跨域允许的请求方式
+    ctx.set('Access-Control-Allow-Methods', 'DELETE,PUT,POST,GET,OPTIONS');
+  }
 
   // 让options尝试请求快速结束
   if (ctx.method.toLowerCase() === 'options') {
